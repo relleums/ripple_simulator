eac39c4d9bb3508ef3a4bdc1bb7f8083b8039066
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2021-06-24 23:39:19 +0200

    explore num res on prefboard

--- examples/register.py
+++ examples/register.py
@@ -2,16 +2,19 @@ import ripple_simulator as ris
 import os
 import subprocess
 
+reg_A = ris.harry_porter_computer.make_register(num_bits=8, ADRBUS=False)
+reg_A = ris.build.add_group_name(circuit=reg_A, name="REG-A")
+reg_A = ris.build.translate(circuit=reg_A, pos=[0, -1])
 
-reg_B = ris.harry_porter_computer.make_register(num_bits=8)
+reg_B = ris.harry_porter_computer.make_register(num_bits=8, ADRBUS=False)
 reg_B = ris.build.add_group_name(circuit=reg_B, name="REG-B")
-reg_B = ris.build.translate(circuit=reg_B, pos=[0, 0])
+reg_B = ris.build.translate(circuit=reg_B, pos=[0, 14])
 
-reg_C = ris.harry_porter_computer.make_register(num_bits=8)
+reg_C = ris.harry_porter_computer.make_register(num_bits=8, ADRBUS=False)
 reg_C = ris.build.add_group_name(circuit=reg_C, name="REG-C")
-reg_C = ris.build.translate(circuit=reg_C, pos=[0, 20])
+reg_C = ris.build.translate(circuit=reg_C, pos=[0, 29])
 
-regs = ris.build.merge_circuits([reg_B, reg_C])
+regs = ris.build.merge_circuits([reg_A, reg_B, reg_C])
 
 seed_mesh_idx = 0
 circuit = ris.compile(circuit=regs)
@@ -19,9 +22,8 @@ meshes, relays, capacitors = ris.compile_relay_meshes(circuit=circuit)
 
 # initial state
 # -------------
-relays["REG-B_bit-01"]["state"] = 1
-relays["REG-B_bit-02"]["state"] = 1
-relays["REG-B_bit-00"]["state"] = 1
+relays["REG-B_bit-01"]["state"] = 5
+relays["REG-B_bit-02"]["state"] = 5
 
 DRAW = True
 
